be416af1230fb4cd7b5c98a44f358dbaf74a2e98
alexschellong <47596717+alexschellong@users.noreply.github.com>
2024-05-07 01:08:56 +0300

    Add files via upload

---
+++ internetimg.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Networking;
+using UnityEngine.Rendering.HighDefinition;
+
+
+public class internetimg : MonoBehaviour
+{
+
+    public Texture textureinternet;
+    public Material Material1;
+    public GameObject Object;
+    public float gOpacity;
+    public float newOpacity;
+    public float tChange;
+    public float tRemain;
+    public float currentTime;
+    public bool moving;
+    public bool changingImg;
+    public Object[] textures;
+    // Start is called before the first frame update)
+
+
+
+    void Start()
+    {
+        textures = Resources.LoadAll("images", typeof(Texture));
+
+        gOpacity = Random.Range(0.0F, 1.0F);
+        newOpacity = Random.Range(0.0F, 1.0F);
+        tChange = Random.Range(0.0F, 5.0F);
+        tRemain = Random.Range(0, 5);
+        currentTime = 0f;
+        moving = true;
+        changingImg = true;
+
+        Object.GetComponent<DecalProjector>().fadeFactor = gOpacity;
+
+        //yGetComponent<DecalProjector>().material.EnableKeyword("_EMISSION");
+        //StartCoroutine(DownloadImage("https://loremflickr.com/1920/1080"));
+
+    }
+
+
+
+
+    void Update()
+    {
+        StartCoroutine(dosomething());
+        StartCoroutine(changeImage());
+
+    }
+
+    IEnumerator dosomething()
+    {
+        if (moving == true)
+        {
+            if (currentTime <= tChange)
+            {
+
+
+                currentTime += Time.deltaTime;
+                Object.GetComponent<DecalProjector>().fadeFactor = Mathf.Lerp(gOpacity, newOpacity, currentTime / tChange);
+            }
+            else
+            {
+                moving = false;
+                Object.GetComponent<DecalProjector>().fadeFactor = newOpacity;
+                gOpacity = newOpacity;
+                newOpacity = Random.Range(0.0F, 1.0F);
+                tChange = Random.Range(0.0F, 5.0F);
+                yield return wait();
+                currentTime = 0f;
+
+            }
+        }
+    }
+    IEnumerator wait()
+    {
+
+        yield return new WaitForSeconds(tRemain);
+        tRemain = Random.Range(0, 5);
+        moving = true;
+    }
+
+    IEnumerator wait2()
+    {
+
+        yield return new WaitForSecondsRealtime(1800);
+        changingImg = true;
+    }
+
+    IEnumerator changeImage()
+    {
+
+        if (changingImg == true)
+        {
+            GetComponent<DecalProjector>().material.EnableKeyword("_EMISSION");
+
+            textureinternet = (Texture)textures[Random.Range(0, textures.Length)];
+
+            GetComponent<DecalProjector>().material.EnableKeyword("_EMISSION");
+            //GetComponent<DecalProjector>().material.SetTexture("_BaseColorMap", textureinternet);
+            Material1.SetTexture("_EmissiveColorMap", textureinternet);
+            Object.GetComponent<DecalProjector>().material = Material1;
+            changingImg = false;
+            yield return wait2();
+        }
+    }
+
+
+
+
+
+    IEnumerator DownloadImage(string MediaUrl)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+            Debug.Log(request.error);
+        else
+            textureinternet = ((DownloadHandlerTexture)request.downloadHandler).texture;
+
+        //GetComponent<DecalProjector>().material.EnableKeyword("_EMISSION");
+        //GetComponent<DecalProjector>().material.SetTexture("_BaseColorMap", textureinternet);
+        Material1.SetTexture("_EmissiveColorMap", textureinternet);
+        Object.GetComponent<DecalProjector>().material = Material1;
+
+    }
+
+    // Update is called once per frame
+
+}
---
+++ videocontrol.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using System.Linq;
+
+
+public class videocontrol : MonoBehaviour
+{
+
+    //Video To Play [Assign from the Editor]
+    public VideoClip videoToPlay;
+    public RenderTexture renderTexture;
+
+    private VideoPlayer videoPlayer;
+    private VideoSource videoSource;
+
+    //Set from the Editor
+    public List<VideoClip> videoClipList;
+
+    private List<VideoPlayer> videoPlayerList;
+    private int videoIndex = 0;
+    Coroutine m_MyCoroutineReference;
+
+    bool notPrepared;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.runInBackground = true;
+
+        m_MyCoroutineReference = StartCoroutine(playVideo());
+    }
+
+
+    IEnumerator playVideo(bool firstRun = true)
+    {
+        if (videoClipList == null || videoClipList.Count <= 0)
+        {
+            Debug.Log("error1");
+            yield break;
+
+
+        }
+
+        notPrepared = true;
+
+        if (firstRun)
+        {
+            var numberList = Enumerable.Range(0, videoClipList.Count).ToList();
+
+            videoPlayerList = new List<VideoPlayer>();
+            for (int i = 0; i < videoClipList.Count; i++)
+            {
+                System.Random rnd = new System.Random();
+                int remove = rnd.Next(numberList.Count - 1);
+
+
+                //Create new Object to hold the Video and the sound then make it a child of this object
+                GameObject vidHolder = new GameObject("VP" + i);
+                vidHolder.transform.SetParent(transform);
+
+
+                //Add VideoPlayer to the GameObject
+                VideoPlayer videoPlayer = vidHolder.AddComponent<VideoPlayer>();
+                videoPlayerList.Add(videoPlayer);
+
+                //Disable Play on Awake for both Video and Audio
+                videoPlayer.playOnAwake = false;
+
+                videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.RenderTexture;
+
+                videoPlayer.targetTexture = renderTexture;
+
+                videoPlayer.audioOutputMode = VideoAudioOutputMode.None;
+
+                //We want to play from video clip not from url
+                videoPlayer.source = VideoSource.VideoClip;
+
+                //Set video Clip To Play 
+                videoPlayer.clip = videoClipList[numberList[remove]];
+
+                numberList.RemoveAt(remove);
+
+            }
+
+            //Prepare video
+            videoPlayerList[videoIndex].Prepare();
+        }
+
+        //Make sure that the NEXT VideoPlayer index is valid
+        if (videoIndex >= videoPlayerList.Count)
+        {
+            Debug.Log("error2");
+            yield break;
+
+
+        }
+
+
+
+
+
+        //Wait until this video is prepared
+        while (!videoPlayerList[videoIndex].isPrepared)
+        {
+            Debug.Log("waiting");
+            yield return null;
+        }
+
+
+        //Play first video
+        videoPlayerList[videoIndex].Play();
+
+        //Wait while the current video is playing
+        bool reachedHalfWay = false;
+        int nextIndex = (videoIndex + 1);
+        while (videoPlayerList[videoIndex].isPlaying)
+        {
+
+
+            //(Check if we have reached half way)
+            if (!reachedHalfWay && videoPlayerList[videoIndex].time >= (videoPlayerList[videoIndex].clip.length / 2))
+            {
+                reachedHalfWay = true; //Set to true so that we don't evaluate this again
+
+                //Make sure that the NEXT VideoPlayer index is valid. Othereise Exit since this is the end
+                if (nextIndex >= videoPlayerList.Count)
+                {
+
+                    Destroy(videoPlayerList[videoIndex]);
+                    videoIndex = 0;
+                    Debug.Log("restart");
+                    StartCoroutine(playVideo());
+                    yield break;
+
+                }
+
+                //Prepare the NEXT video
+                Debug.Log("preparing");
+                Debug.Log((videoPlayerList[nextIndex]).clip);
+                notPrepared = false;
+                videoPlayerList[nextIndex].Prepare();
+            }
+            yield return null;
+        }
+        Destroy(videoPlayerList[videoIndex]);
+        Debug.Log("waiting2here it happened" + nextIndex + " ");
+        Debug.Log((videoPlayerList[nextIndex]).clip);
+        //Wait until NEXT video is prepared
+        while (!videoPlayerList[nextIndex].isPrepared)
+        {
+            if (notPrepared)
+            {
+
+                videoPlayerList[nextIndex].Prepare();
+                notPrepared = false;
+            }
+            yield return null;
+        }
+
+
+
+        //Increment Video index
+        videoIndex++;
+
+
+        //Play next prepared video. Pass false to it so that some codes are not executed at-all
+        StartCoroutine(playVideo(false));
+
+    }
+
+
+
+
+
+
+
+}
